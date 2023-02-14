a4c2419f1c165d3742659d79e2066b3e444a20ed
Scott Ritchie <sritchie73@gmail.com>
2023-02-14 10:41:53 +0000

    added new PGS

--- src/07_INTERVAL_replication/02_download_comparison_PGS.sh
+++ src/07_INTERVAL_replication/02_download_comparison_PGS.sh
@@ -28,3 +28,12 @@ mkdir -p data/T2D_PGS/UKB_invalid
 ./src/PGS_resources/download_PGS_from_catalog.sh --nouuid --overwrite \
   --pgsid PGS002026 --name Prive2022_LDpred2 --out data/T2D_PGS/UKB_invalid/Prive2022_LDpred2
 
+./src/PGS_resources/download_PGS_from_catalog.sh --nouuid --overwrite \
+  --pgsid PGS003103 --name Ma2022 --out data/T2D_PGS/UKB_invalid/Ma2022
+
+./src/PGS_resources/download_PGS_from_catalog.sh --nouuid --overwrite \
+  --pgsid PGS002780 --name Wong2022 --out data/T2D_PGS/UKB_invalid/Wong2022
+
+./src/PGS_resources/download_PGS_from_catalog.sh --nouuid --overwrite \
+  --pgsid PGS002671 --name Weissbrod2022 --out data/T2D_PGS/UKB_invalid/Weissbrod2022
+
