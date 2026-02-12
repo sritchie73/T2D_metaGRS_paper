130a5cb34f99ea5690ac0486668267e60796ce60
Dr. Scott C. Ritchie <sr827@medschl.cam.ac.uk>
2026-02-12 16:05:33 +0000

    Dispatch script

---
+++ src/calc_PRS_UKB/03_calc_all_PRSs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+bash src/PGS_resources/UKB_RAP/calc_PS_lvls.sh \
+  --score-file 'PRS_score_files' \
+  --type 'd' \
+  --out 'PRS_levels' \
+  --single-out 'all_PRSs.sscore.gz' \
+  --work 'PRS_levels/log'
