0fe856cf6a13ecef93143cacee017cf2f43c24b6
Dr. Scott C. Ritchie <sr827@medschl.cam.ac.uk>
2026-02-23 17:03:17 +0000

    Cohort characteristics for 10-year risk in INTERVAL

--- src/validation_INTERVAL/03_cohort_characteristics.R
+++ src/validation_INTERVAL/03_cohort_characteristics.R
@@ -30,8 +30,51 @@ pheno[all_pheno, on = .(identifier), smoker := fcase(
 # Curate sex
 pheno[, sex := ifelse(sex == 2, "Female", "Male")]
 
-
 # Compute cohort characteristics
+cc <- data.table(
+  "sample_size_cases"=sprintf("%s (%s/%s)",
+    format(pheno[(incident_diabetes), .N], big.mark=","),
+    format(pheno[(incident_diabetes) & sex == "Male", .N], big.mark=","),
+    format(pheno[(incident_diabetes) & sex == "Female", .N], big.mark=",")
+  ),
+  "sample_size_controls"=sprintf("%s (%s/%s)",
+    format(pheno[!(incident_diabetes), .N], big.mark=","),
+    format(pheno[!(incident_diabetes) & sex == "Male", .N], big.mark=","),
+    format(pheno[!(incident_diabetes) & sex == "Female", .N], big.mark=",")
+  ),
+  "age_cases"=sprintf("%s (%s)",
+    pheno[(incident_diabetes), round(mean(age), digits=1)],
+    pheno[(incident_diabetes), round(sd(age), digits=1)]
+  ),
+  "age_controls"=sprintf("%s (%s)",
+    pheno[!(incident_diabetes), round(mean(age), digits=1)],
+    pheno[!(incident_diabetes), round(sd(age), digits=1)]
+  ),
+  "bmi_cases"=sprintf("%s (%s)",
+    pheno[(incident_diabetes) & !is.na(bmi), round(mean(bmi), digits=1)],
+    pheno[(incident_diabetes) & !is.na(bmi), round(sd(bmi), digits=1)]
+  ),
+  "bmi_controls"=sprintf("%s (%s)",
+    pheno[!(incident_diabetes) & !is.na(bmi), round(mean(bmi), digits=1)],
+    pheno[!(incident_diabetes) & !is.na(bmi), round(sd(bmi), digits=1)]
+  ),
+  "smokers_cases"=sprintf("%s (%s/%s)",
+    format(pheno[(incident_diabetes) & (smoker), .N], big.mark=","),
+    format(pheno[(incident_diabetes) & (smoker) & sex == "Male", .N], big.mark=","),
+    format(pheno[(incident_diabetes) & (smoker) & sex == "Female", .N], big.mark=",")
+  ),
+  "smokers_controls"=sprintf("%s (%s/%s)",
+    format(pheno[!(incident_diabetes) & (smoker), .N], big.mark=","),
+    format(pheno[!(incident_diabetes) & (smoker) & sex == "Male", .N], big.mark=","),
+    format(pheno[!(incident_diabetes) & (smoker) & sex == "Female", .N], big.mark=",")
+  )
+)
+fwrite(cc, sep="\t", quote=FALSE, file="output/INTERVAL_tests/case_control_cohort_characteristics.txt")
+
+# Compute cohort characteristics for 10 year T2D risk prediction
+# Truncate follow-up to 10-years
+pheno[!(incident_diabetes), incident_censor_years := pmin(10, incident_censor_years)]
+pheno[(incident_diabetes) & incident_censor_years > 10, c("incident_diabetes", "incident_censor_years") := .(FALSE, 10)]
 cc <- data.table(
   "sample_size_cases"=sprintf("%s (%s/%s)",
     format(pheno[(incident_diabetes), .N], big.mark=","),
@@ -78,4 +121,4 @@ cc <- data.table(
     format(pheno[!(incident_diabetes) & (smoker) & sex == "Female", .N], big.mark=",")
   )
 )
-fwrite(cc, sep="\t", quote=FALSE, file="output/INTERVAL_tests/cohort_characteristics.txt")
+fwrite(cc, sep="\t", quote=FALSE, file="output/INTERVAL_tests/incident_t2d_cohort_characteristics.txt")
